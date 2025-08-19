c723eeb64ccc69ece80b54f3101936749220d1e1
Emmanuelf21 <emmanuelfranco2001@gmail.com>
2025-08-19 20:57:19 -0300

    views

--- receitas/views.py
+++ receitas/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # receitas/views.py 
 def home(request): 
     return render(request, 'receitas/home.html') 
+
+def receita_detail(request, id):
+    context = {
+        'receita_id': id,
+        'receita_title': f'Receita Detalhada {id}',
+        'receita_description': f'Esta é uma descrição detalhada da receita com ID {id}.'
+    }
+    return render(request, 'receitas/receita_detail.html', context)
