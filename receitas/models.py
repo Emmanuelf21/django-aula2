d7e2fb525e4c506486e3ba293cce6926164ddcac
Emmanuelf21 <emmanuelfranco2001@gmail.com>
2025-08-20 20:35:36 -0300

    refiz o banco de dados

--- receitas/models.py
+++ receitas/models.py
@@ -5,7 +5,7 @@ class Receita(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     ingredients = models.TextField()
-    ingredients = models.TextField()
+    instructions = models.TextField()
     image = models.ImageField(upload_to='receitas/images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
