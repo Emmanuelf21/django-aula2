bd377b2bbf0221ca24f62a0afaa9c2d75844ee48
Emmanuelf21 <emmanuelfranco2001@gmail.com>
2025-09-10 21:51:14 -0300

    adicionei a pagina de bebidas e sobremesa

--- receitas/views.py
+++ receitas/views.py
@@ -3,8 +3,16 @@ from .models import Receita
 
 # receitas/views.py 
 def home(request): 
-    receitas = Receita.objects.all()
-    return render(request, 'receitas/home.html', {'receitas':receitas}) 
+    categoria_slug = request.GET.get('categoria')
+    categorias_choices = [choice[0] for choice in Receita.CATEGORIAS]
+    if categoria_slug:
+        receitas = Receita.objects.filter(categoria=categoria_slug)
+        categoria_selecionada = categoria_slug
+    else:
+        receitas = Receita.objects.all()
+        categoria_selecionada = None
+        
+    return render(request, 'receitas/home.html', {'receitas':receitas, 'categoria': categorias_choices, 'categoria_selecionada': categoria_selecionada}) 
 
 def receita_detail(request, id):
     receita = get_object_or_404(Receita, pk=id)
